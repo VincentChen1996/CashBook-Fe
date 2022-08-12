612c762a50630f62eb9eaa8c3a25483d1ef21ee5
Vincent <853361280@qq.com>
2022-08-12 08:09:42 +0800

    second commit

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'juejue-vite-h5',
+      script: 'juejue-vite-h5-server.js'
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'root',
+      host: '106.14.208.52',
+      ref: 'origin/master',
+      repo: 'git@github.com:VincentChen1996/CashBook-Fe.git',
+      path: '/workspace/CashBook-Fe',
+      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  }
+}
